8846de42efed91cc4a6da96876444380d1a872cc
Lautaro Galman <74028143+LautaG@users.noreply.github.com>
2021-08-13 18:12:35 -0300

    Add files via upload

---
+++ js/modales.js
@@ -0,0 +1,22 @@
+function modalContacto() {
+  var element = document.getElementById("modal-contacto");
+  element.classList.add("is-active");
+  var element = document.getElementById("htmlpadre");
+   element.classList.add("is-clipped");
+}
+function modalContactoClose() {
+  var element = document.getElementById("modal-contacto");
+  element.classList.remove("is-active");
+    element.classList.remove("is-clipped");
+   
+
+}
+function sub() {
+  document.getElementById("info-cta-sub").style.display = "none";
+    document.getElementById("info-cta-sub-done").style.display = "block";
+    document.getElementsByClassName("screen-reader-response").style.display = "none";
+}
+function contacto() {
+ document.getElementById("pop-up-form").style.display = "none";
+    document.getElementById("pop-up-exito").style.display = "block";
+}
